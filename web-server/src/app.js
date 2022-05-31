f3e6075e76500f972e888380b416d0fe6c2bf350
godxvincent <godxvincent@gmail.com>
2022-05-30 20:19:25 -0500

    :construction: Work in progress

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,14 @@
+const path = require("path");
 const express = require("express");
 
-const app = express();
+const publicDirectory = path.join(__dirname, "../public");
 
-app.get("", (request, response) => {
-  response.send("Hello Express!");
-});
+const app = express();
+app.use(express.static(publicDirectory));
+
+// app.get("/weather", (request, response) => {
+//   response.send("weather Page");
+// });
 
 app.listen(3000, () => {
   console.log("Server is runing on port 3000");
