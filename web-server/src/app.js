c5fd65c63a2b43e87a41fcdcb25a7d9ef19a3f93
godxvincent <godxvincent@gmail.com>
2022-06-04 12:54:12 -0500

    :sparkles: Adding new features to web-server project

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const publicDirectory = path.join(__dirname, "../public");
 
 const app = express();
+// Set allows to define different settings for express in this case we are setting hbs as
+// view engine.
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectory));
 
 // app.get("/weather", (request, response) => {
